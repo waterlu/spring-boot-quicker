a89f00d4568a9d055edb499254833c883b421127
鲁铁华 <lutiehua@zj-inv.cn>
2017-09-25 11:26:07 +0800

    更改样式

--- src/main/resources/static/js/controllers/project.js
+++ src/main/resources/static/js/controllers/project.js
@@ -13,6 +13,28 @@ angular.module('app').controller('ProjectController', ['$scope', '$http', '$stat
     $scope.springBootVersion = '1.5.6.RELEASE';
     $scope.dependencies = [];
 
+    $scope.dbType = 'MySQL';
+    $scope.dbIP = '10.10.10.4';
+    $scope.dbPort = '3306';
+    $scope.dbName = 'kingold';
+    $scope.dbUsername = 'zj_admin';
+    $scope.dbPassword = '123456';
+    $scope.maxRows = 100;
+
+    $scope.tables = [];
+    $scope.fields = [];
+
+    $scope.row_status = [];
+    for (var i = 0; i < $scope.maxRows; i++) {
+        $scope.row_status[i] = true;
+    }
+
+    $scope.resetRowStatus = function () {
+        for (var i = 0; i < $scope.row_status.length; i++) {
+            $scope.row_status[i] = true;
+        }
+    };
+
     $scope.initDefaultDependency = function() {
 
         var fastjson = [];
@@ -176,6 +198,118 @@ angular.module('app').controller('ProjectController', ['$scope', '$http', '$stat
         }).error(function (data) {
             console.log('data=' + data);
         });
+    };
+
+    $scope.testDBConnection = function () {
+        var param = {
+            'dbType': null,
+            'dbIP': null,
+            'dbPort': null,
+            'dbName': null,
+            'dbUsername': null,
+            'dbPassword': null
+        };
+
+        param.dbType = $scope.dbType;
+        param.dbIP = $scope.dbIP;
+        param.dbPort = $scope.dbPort;
+        param.dbName = $scope.dbName;
+        param.dbUsername = $scope.dbUsername;
+        param.dbPassword = $scope.dbPassword;
+
+        var jsonString = JSON.stringify(param);
+        console.log(jsonString);
+
+        $http.post("/api/database/connect", jsonString).success(function(data) {
+            console.log('code=' + data.code);
+            console.log('data=' + data.data);
+            if (data.code == 200) {
+                window.alert("成功！")
+            } else {
+                window.alert(data.message)
+            }
+        }).error(function (data) {
+            console.log('data=' + data);
+        });
+    };
+
+    $scope.readTables = function () {
+        var param = {
+            'dbType': null,
+            'dbIP': null,
+            'dbPort': null,
+            'dbName': null,
+            'dbUsername': null,
+            'dbPassword': null
+        };
+
+        param.dbType = $scope.dbType;
+        param.dbIP = $scope.dbIP;
+        param.dbPort = $scope.dbPort;
+        param.dbName = $scope.dbName;
+        param.dbUsername = $scope.dbUsername;
+        param.dbPassword = $scope.dbPassword;
+
+        var jsonString = JSON.stringify(param);
+        console.log(jsonString);
+
+        $http.post("/api/database/tables", jsonString).success(function(data) {
+            if (data.code == 200) {
+                $scope.resetRowStatus();
+                $scope.tables = data.data;
+            } else {
+                window.alert(data.message)
+            }
+        }).error(function (data) {
+            console.log('data=' + data);
+        });
+    };
+
+    $scope.select = function (index, table) {
+        if (!$scope.row_status[index]) {
+            $scope.row_status[index] = !$scope.row_status[index];
+        } else {
+            var param = {
+                'dbType': null,
+                'dbIP': null,
+                'dbPort': null,
+                'dbName': null,
+                'dbUsername': null,
+                'dbPassword': null
+            };
+
+            param.dbType = $scope.dbType;
+            param.dbIP = $scope.dbIP;
+            param.dbPort = $scope.dbPort;
+            param.dbName = $scope.dbName;
+            param.dbUsername = $scope.dbUsername;
+            param.dbPassword = $scope.dbPassword;
+
+            var jsonString = JSON.stringify(param);
+            console.log(jsonString);
+            var tableName = table.name;
+            $http.post("/api/database/tables/" + tableName, jsonString).success(function(data) {
+                if (data.code == 200) {
+                    $scope.resetRowStatus();
+                    $scope.fields = data.data;
+                    $scope.row_status[index] = !$scope.row_status[index];
+                } else {
+                    window.alert(data.message)
+                }
+            }).error(function (data) {
+                console.log('data=' + data);
+            });
+        }
+    };
+
+    $scope.selectPrimaryFiled = function(index) {
+        var isPrimaryKey = $scope.fields[index].isPrimaryKey;
+        console.log('isPrimaryKey=' + isPrimaryKey);
+        if (isPrimaryKey) {
+            return "active";
+        } else {
+            return "";
+        }
     };
 
 }]);
